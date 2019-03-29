00c4090ab672dd6f19e075d6bf9c23bc75590b08
Umair <omsolutionpk@gmail.com>
2019-03-29 14:46:32 +0500

    quick fix

--- app/services/spree/compare_line_items_decorator.rb
+++ app/services/spree/compare_line_items_decorator.rb
@@ -3,6 +3,8 @@ module Spree
     def compare(_line_item, _options)
       if _options["ad_hoc_option_value_ids"].present?
         _line_item.ad_hoc_option_value_ids.map(&:to_s) == _options["ad_hoc_option_value_ids"].reject{|o|o==""}
+      else
+        true
       end
     end
   end
