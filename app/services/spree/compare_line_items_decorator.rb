45010efb7a750709ab06a4a311896ebf089fba30
Umair <omsolutionpk@gmail.com>
2019-04-03 04:39:57 +0500

    changed the ways I was making array comparison.

--- app/services/spree/compare_line_items_decorator.rb
+++ app/services/spree/compare_line_items_decorator.rb
@@ -1,11 +1,14 @@
 module Spree
   CompareLineItems.class_eval do
     def compare(_line_item, _options)
-      if _options["ad_hoc_option_value_ids"].present?
-        _line_item.ad_hoc_option_value_ids.map(&:to_s) == _options["ad_hoc_option_value_ids"].reject{|o|o==""}
-      else
-        true
+      opts = _options["ad_hoc_option_value_ids"].compact.reject{|o|o==""}
+      ret = true
+
+      if opts.present?
+        ret = _line_item.ad_hoc_option_value_ids.map(&:to_s).all?{|obj|opts.include?(obj)}
       end
+
+      ret
     end
   end
 end
