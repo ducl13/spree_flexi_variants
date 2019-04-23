50b21752f841f1e145f81b2ade98917a034b2055
Duc T. Lam <ducl_13@icloud.com>
2019-04-23 16:35:06 -0700

    Overrides for spree_html_invoice views

--- app/overrides/add_extra_order_details_line_item_description.rb
+++ app/overrides/add_extra_order_details_line_item_description.rb
@@ -12,3 +12,33 @@ Deface::Override.new(
   partial: "spree/shared/extra_order_details_line_item_description"
 )
 
+### START spree_html_invoice overrides
+
+Deface::Override.new(
+  virtual_path: "spree/admin/invoice/_line_items_invoice",
+  name: "add_extra_order_details_line_item_description_admin_line_items_invoice",
+  insert_bottom: "[data-hook='line_items_invoice'] td:nth-of-type(2)",
+  partial: "spree/shared/extra_order_details_line_item_description"
+)
+
+Deface::Override.new(
+  virtual_path: "spree/admin/invoice/_line_items_packaging_slip",
+  name: "add_extra_order_details_line_item_description_admin_line_items_packaging_slip",
+  insert_bottom: "[data-hook='line_items_packaging_slip'] td:nth-of-type(2)",
+  partial: "spree/shared/extra_order_details_line_item_description"
+)
+
+Deface::Override.new(
+  virtual_path: "spree/invoice_mailer/_line_items_invoice",
+  name: "add_extra_order_details_line_item_description_mailer_line_items_invoice",
+  insert_bottom: "[data-hook='line_items_invoice'] td:nth-of-type(2)",
+  partial: "spree/shared/extra_order_details_line_item_description"
+)
+
+Deface::Override.new(
+  virtual_path: "spree/invoice_mailer/_line_items_packaging_slip",
+  name: "add_extra_order_details_line_item_description_mailer_line_items_packaging_slip",
+  insert_bottom: "[data-hook='line_items_packaging_slip'] td:nth-of-type(2)",
+  partial: "spree/shared/extra_order_details_line_item_description"
+)
+### END spree_html_invoice overrides
